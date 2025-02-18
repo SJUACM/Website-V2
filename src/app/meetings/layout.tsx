6d8e2cc1baace1f217e629c5cbf7644e1a7f0fa5
hinnazeejah <hinnazeejahhh@gmail.com>
2025-02-18 14:59:39 -0500

    update meetings page

--- src/app/meetings/layout.tsx
+++ src/app/meetings/layout.tsx
@@ -10,18 +10,17 @@ export default function Layout({
   return (
     <main className="flex flex-col items-center justify-between p-4 md:p-24">
       <div className="z-50 w-full max-w-5xl items-center justify-between font-poppins text-sm lg:flex">
-        <div className="flex h-24 w-full justify-center lg:static lg:size-auto">
+        <div className="flex h-12 w-full justify-center lg:static lg:size-auto">
           <Navbar />
-          <a
-            className="flex place-items-center gap-2 lg:pointer-events-auto lg:p-0"
-            href="/"
-          >
-          </a>
         </div>
       </div>
 
-      <div className="pt-8 md:pt-8">
-        <div className="relative z-[-1] flex place-items-center before:absolute before:h-[400px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[280px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#de2307] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[340px] before:lg:h-[360px] text-4xl text-center"></div>
+      <div className="pt-20 md:pt-20">
+        <div className="text-center mb-8 md:mb-4">
+          <h1 className="text-4xl font-semibold">Meetings</h1>
+        </div>
+
+        <div className="relative z-[-1] flex place-items-center before:absolute before:h-[200px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[120px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#de2307] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[340px] before:lg:h-[260px]"></div>
 
         {children}
       </div>
--- src/app/meetings/page.tsx
+++ src/app/meetings/page.tsx
@@ -29,7 +29,6 @@ export default function MeetingsPage() {
   return (
     <div className="text-center items-center justify-center max-w-7xl mx-auto px-8">
       <div className="p-8">
-        <h1 className="text-4xl font-bold mb-8 text-white">Meetings</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {meetings.map((meeting) => (
             <Meeting
