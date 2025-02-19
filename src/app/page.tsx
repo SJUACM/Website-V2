d1b960ad6c04f059dd94e37669769b2dc0ac2d2c
hinnazeejah <hinnazeejahhh@gmail.com>
2025-02-18 21:54:21 -0500

    updated home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,12 @@ import InfoSection from "./components/info-section";
 import LeadershipSection from "./components/leadership-section";
 import ResourcesSection from "./components/resources-section";
 import styles from './styles/customFont.module.css'
+import { Comfortaa } from "next/font/google";
+
+const comfortaa = Comfortaa({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function Home() {
   return (
@@ -20,15 +26,22 @@ export default function Home() {
       </div>
 
       <div className="pt-24 md:pt-48 space-y-4 text-center w-full px-4 md:px-0">
-        {/* Desktop Title */}
-        <h1 className={`hidden md:block text-4xl font-semibold ${styles.customFont}`}>
-          St. John&apos;s University ACM Student Chapter
-        </h1>
-        
-        {/* Mobile Title */}
-        <h1 className="block md:hidden text-3xl font-semibold">
-          St. John&apos;s University
-        </h1>
+        {/* Desktop Title Structure */}
+        <div className="hidden md:flex flex-col items-center space-y-6">
+          <h2 className={`text-2xl font-normal ${comfortaa.className}`}>
+            St. John&apos;s University
+          </h2>
+          <h1 className={`text-5xl font-bold ${styles.customFont}`}>
+            ACM Student Chapter
+          </h1>
+        </div>
+
+        {/* Mobile Title Structure */}
+        <div className="flex md:hidden flex-col items-center space-y-4">
+          <h2 className={`text-xl font-normal ${comfortaa.className}`}>
+            St. John&apos;s University
+          </h2>
+        </div>
 
         <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] md:before:h-[400px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] md:after:h-[280px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#de2307] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[340px] before:lg:h-[360px] text-4xl text-center"></div>
 
