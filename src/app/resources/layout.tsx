c8b3d36198b86f3e64c1b6a435c50f38df85e5d7
Tomas Santos Yciano <tomassantos484@gmail.com>
2025-02-19 00:05:26 -0500

    im tired pt 3

--- src/app/resources/layout.tsx
+++ src/app/resources/layout.tsx
@@ -4,15 +4,19 @@ import React from "react";
 import { Navbar } from "../components/navbar";
 import styles from '../styles/customFont.module.css'
 
+interface ResourceParams {
+  section?: string;
+}
+
 type LayoutProps = {
   children: React.ReactNode;
-  params: Promise<{ section?: string }> | { section?: string };
+  params?: Promise<ResourceParams>;
 };
 
-export default function Layout({ children, params }: LayoutProps) {
-  const section = 'then' in params ? undefined : params.section;
-  
-  const getTitle = () => {
+export default async function Layout({ children, params }: LayoutProps) {
+  const getTitle = async () => {
+    const section = params ? (await params).section : undefined;
+    
     switch (section) {
       case 'interview-prep':
         return 'Interview Prep';
@@ -25,6 +29,8 @@ export default function Layout({ children, params }: LayoutProps) {
     }
   };
 
+  const title = await getTitle();
+
   return (
     <main className="flex flex-col items-center justify-between p-4 md:p-24">
       <div className="z-50 w-full max-w-5xl items-center justify-between font-poppins text-sm lg:flex">
@@ -35,7 +41,7 @@ export default function Layout({ children, params }: LayoutProps) {
 
       <div className="pt-20 md:pt-20">
       <div className="text-center mb-8 md:mb-4">
-          <h1 className={`text-4xl font-semibold ${styles.customFont}`}>{getTitle()}</h1>
+          <h1 className={`text-4xl font-semibold ${styles.customFont}`}>{title}</h1>
        </div>
 
         <div className="relative z-[-1] flex place-items-center before:absolute before:h-[200px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[120px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#de2307] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[340px] before:lg:h-[260px]"></div>
