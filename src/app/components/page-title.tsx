17e7fab743a987111c3494767a334a584f27f354
Faizan Ahmed <faizanahmed80113@gmail.com>
2025-03-13 23:49:40 -0400

    remove unused files

--- src/app/components/page-title.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function PageTitle({ children }: { children: React.ReactNode }) {
-  return (
-    <h1 className="text-4xl font-bold text-center text-white mb-8">
-      {children}
-    </h1>
-  );
-}
